a0e00777bbbc40e025f08c10fcd88a3865f933f1
KohnoA <san.kohno@yandex.ru>
2022-07-04 22:59:23 +0300

    feat: readable number before 20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function toReadable (number) {
-  
+  const textBeforeTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+  let result = '';
+
+  for(let i = 0; i <= number; i++) {
+    if(number > 20) {
+      result = `${textBeforeTwenty[20]}${toReadable(textBeforeTwenty[i])}`;
+    } else {
+      result = textBeforeTwenty[i];
+    }
+  }
+
+  return result;
 }
+
+
+console.log(module.exports(21));
