874d30a5a1c82461084dc7d86edc1e8f8af353b5
KohnoA <san.kohno@yandex.ru>
2022-07-05 13:09:44 +0300

    refactor: code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,42 @@
 module.exports = function toReadable (number) {
-  const textBeforeTwenty = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
-    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+  const numBeforeNineteen = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten',
+    'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+  const numBeforeNinetyNine = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty','ninety'];
+
+  const numBeforeNineHundredNinetyNine = ['hundred'];
+
   let result = '';
 
-  for(let i = 0; i <= number; i++) {
-    if(number > 20) {
-      result = `${textBeforeTwenty[20]}${toReadable(textBeforeTwenty[i])}`;
-    } else {
-      result = textBeforeTwenty[i];
+  if(number < 20) {
+    for(let i = 0; i <= number; i++) {
+      result = numBeforeNineteen[i];
     }
+
+
+  } else if(number < 100) {
+    current = Math.trunc(number / 10);
+    for(let i = 0; i <= current; i++) {
+      if(number % 10 === 0) {
+        result = numBeforeNinetyNine[i - 2];
+      } else {
+        result = numBeforeNinetyNine[i - 2] + ' one';
+      }
+    }
+
+
+  } else if(number < 1000) {
+    for(let i = 0; i <= number; i++) {
+      result = numBeforeNineHundredNinetyNine[0];
+    }
+
+
+  } else {
+    return 'WARNING: Incorrect number entered';
   }
 
   return result;
 }
 
 
-console.log(module.exports(21));
+console.log(module.exports(30));
